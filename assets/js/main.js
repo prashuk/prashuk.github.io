d5ba612ce71c63d6c752a35b232eef1f0c29e91b
Prashuk <prashuk.ajmera@gmail.com>
2020-12-17 01:29:14 -0600

    Map and background svg added

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,15 @@
 
   $(".scrolly").scrolly();
 })(jQuery);
+
+function initMap() {
+  const loc = { lat: 32.948334, lng: -96.729851 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: loc,
+  });
+  const marker = new google.maps.Marker({
+    position: loc,
+    map: map,
+  });
+}
